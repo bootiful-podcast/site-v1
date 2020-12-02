3b39bd0cfd256e76ed61fba20d94c26d7215c430
Josh Long <josh@joshlong.com>
2020-12-01 17:44:49 -0800

    overlays for kustomize

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,7 +9,7 @@ export ROOT_DIR=$(cd $(dirname $0) && pwd)
 export OD=${ROOT_DIR}/overlays/${BP_MODE_LOWERCASE}
 export APP_NAME=site
 export PROJECT_ID=${GCLOUD_PROJECT}
-
+export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 
 echo "The ENV_SUB_DOMAIN=${ENV_SUB_DOMAIN}"
 echo "BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE}"
@@ -18,12 +18,12 @@ echo "BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE}"
 cd $(dirname $0)/..
 ROOT_DIR=$(pwd)
 cd $ROOT_DIR
-rm $PROD_ENV_FILE
 
 rm -rf $ROOT_DIR/build
 rm -rf $ROOT_DIR/dist
 
 PROD_ENV_FILE=${ROOT_DIR}/.env.production
+rm $PROD_ENV_FILE
 touch $PROD_ENV_FILE
 echo "VUE_APP_SERVICE_ROOT=https://api.${ENV_SUB_DOMAIN}bootifulpodcast.fm" >> ${PROD_ENV_FILE}
 echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >> ${PROD_ENV_FILE}
@@ -44,15 +44,12 @@ cd $ROOT_DIR/build
 pack build $APP_NAME --builder paketobuildpacks/builder:full --buildpack gcr.io/paketo-buildpacks/nginx:latest  --env PORT=8080
 image_id=$(docker images -q $APP_NAME)
 
-GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 echo "pushing ${image_id} to gcr.io/${PROJECT_ID}/${APP_NAME}"
 echo "tagging ${GCR_IMAGE_NAME}"
 docker tag "${image_id}" ${GCR_IMAGE_NAME}
 echo "finished tag"
 docker push ${GCR_IMAGE_NAME}
 echo "finished push"
-
-
 
 export RESERVED_IP_NAME=site-${BP_MODE_LOWERCASE}-ip
 gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
