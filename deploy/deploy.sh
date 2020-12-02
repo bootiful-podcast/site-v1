c82cc32eeceab0389ceebbafd1fb039eb2cd2650
Josh Long <josh@joshlong.com>
2020-12-01 17:32:58 -0800

    overlays for kustomize

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,12 +5,15 @@ set -o pipefail
 
 export BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE:-development}
 export ENV_SUB_DOMAIN=$( [ "${BP_MODE_LOWERCASE}" = "production" ] && echo ""  || echo "${BP_MODE_LOWERCASE}.")
+export ROOT_DIR=$(cd $(dirname $0) && pwd)
+export OD=${ROOT_DIR}/overlays/${BP_MODE_LOWERCASE}
+export APP_NAME=site
+export PROJECT_ID=${GCLOUD_PROJECT}
+
 
 echo "The ENV_SUB_DOMAIN=${ENV_SUB_DOMAIN}"
 echo "BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE}"
 
-export APP_NAME=site
-export PROJECT_ID=${GCLOUD_PROJECT}
 
 cd $(dirname $0)/..
 ROOT_DIR=$(pwd)
@@ -20,11 +23,13 @@ rm -rf $ROOT_DIR/build
 rm -rf $ROOT_DIR/dist
 
 PROD_ENV_FILE=${ROOT_DIR}/.env.production
+
 rm $PROD_ENV_FILE
+
 touch $PROD_ENV_FILE
 echo "VUE_APP_SERVICE_ROOT=https://api.${ENV_SUB_DOMAIN}bootifulpodcast.fm" >> ${PROD_ENV_FILE}
-echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >>  ${PROD_ENV_FILE}
-echo "VUE_APP_BP_MODE=${BP_MODE_LOWERCASE}" >>  ${PROD_ENV_FILE}
+echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >> ${PROD_ENV_FILE}
+echo "VUE_APP_BP_MODE=${BP_MODE_LOWERCASE}" >> ${PROD_ENV_FILE}
 
 echo "--------------------------"
 echo "The production environment file contains: "
@@ -46,11 +51,14 @@ echo "pushing ${image_id} to gcr.io/${PROJECT_ID}/${APP_NAME}"
 echo "tagging ${GCR_IMAGE_NAME}"
 docker tag "${image_id}" ${GCR_IMAGE_NAME}
 echo "finished tag"
-
 docker push ${GCR_IMAGE_NAME}
 echo "finished push"
 
-kubectl apply -f ${ROOT_DIR}/deploy/deployment.yaml
-kubectl patch deployment $APP_NAME -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$(date +%s)\"}}}}}"
 
-kubectl apply -f ${ROOT_DIR}/deploy/deployment-service.yaml
+
+#kubectl apply -f ${ROOT_DIR}/deploy/deployment.yaml
+#kubectl apply -f ${ROOT_DIR}/deploy/deployment-service.yaml
+
+
+kubectl apply -k ${OD}
+#kubectl patch deployment $APP_NAME -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": { \"redeploy\": \"$(date +%s)\"}}}}}"
